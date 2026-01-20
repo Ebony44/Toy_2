04500082d6792ff1467fee13ffa9bf2b1c4c9303
Sandalwood <ebonytroll@gmail.com>
2026-01-20 13:01:04 +0900

    adding package for distributed authority

--- Assets/Scripts/ManagedObjectVisualManager.cs
+++ Assets/Scripts/ManagedObjectVisualManager.cs
@@ -1,7 +1,13 @@
+using System;
 using UnityEngine;
 
 public class ManagedObjectVisualManager : MonoBehaviour
 {
+    // pickups, collectibles
+    // projectiles, bullets
+    // destructible environment
+    // visual effects
+
     public static ManagedObjectVisualManager Instance;
     private void Awake()
     {
@@ -13,5 +19,12 @@ public class ManagedObjectVisualManager : MonoBehaviour
         {
             Instance = this;
         }
+        // Time.frameCount
+        var theta = Time.frameCount / 10.0f;
+        transform.position = new Vector3((float)Math.Cos(theta), 0.0f, (float)Math.Sin(theta));
+        // at 0 degree, cos = 1, sin = 0
+        // at 90 degree, cos = 0, sin = 1
+        // at 180 degree, cos = -1, sin = 0
+        // at 270 degree, cos = 0, sin = -1
     }
 }
